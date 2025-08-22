cc6832f1db4a4bcc31dc0cfa835b91ba941be4cf
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2025-08-22 13:45:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin:  [ `${process.env.FRONTEND_URL}` , "http://localhost:5173" ],
     exposedHeaders: ['X-Total-Count'],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
